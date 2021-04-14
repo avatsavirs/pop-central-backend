b93aa059589c9ebfd7790d801ba5b232c4631f08
Kartik <kartikey.sri98@gmail.com>
2021-04-14 12:21:44 +0530

    change: mongoose user schema

--- src/database_models/User.js
+++ src/database_models/User.js
@@ -1,7 +1,24 @@
 import {Schema, model} from 'mongoose';
 
-const UserSchema = new Schema();
+const ListSchema = new Schema({
+  title: String,
+  listItems: [{
+    title: String,
+    url: String,
+    externalId: String,
+    mediaType: String
+  }],
+});
 
-const User = model('user', UserSchema, 'users');
+const UserSchema = new Schema({
+  providerId: String,
+  name: String,
+  email: String,
+  provider: String,
+  image: String,
+  lists: [ListSchema]
+});
+
+const User = model('user', UserSchema);
 
 export default User;
